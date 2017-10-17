7260d0b9dcfb98a2ae85a9354f0f88838c7f7e8e
Ravi Shivam Autar <ravi_autar27@hotmail.com>
2017-10-17 15:22:14 +0200

    Add filter for pauses

--- audio_processor/processor.py
+++ audio_processor/processor.py
@@ -1,9 +1,31 @@
 import subprocess
 import os
 
+data = "../data_audio/banana_dance.wav"
+def extract_audio():
+    for root, dirs, files in os.walk("../data_video/"):
+        for f in files:
+            command = "ffmpeg -i {}{} -ab 160k -ac 2 -ar 44100 -vn ../data_audio/{}.wav".format(root, f, f[:-4])
+            subprocess.call(command, shell=True)
 
-for root, dirs, files in os.walk("../video_files/"):
-    for f in files:
-        command = "ffmpeg -i {}{} -ab 160k -ac 2 -ar 44100 -vn ../audio_files/{}.wav".format(root, f, f)
-        subprocess.call(command, shell=True)
 
+from scipy.io import wavfile
+from matplotlib import pyplot as plt
+import numpy as np
+
+# Load the data and calculate the time of each sample
+samplerate, data = wavfile.read('../data_audio/banana_dance.wav')
+times = np.arange(len(data))/float(samplerate)
+timestep = times[1]
+
+# data = data[:, 0]
+silences = np.array([0 if (-1000<x<1000) else x for x in data[:, 0]])
+print "time (seconds) in total: {}".format(len(times)*timestep)
+print "time (seconds) with pauses: {}".format(sum(silences==0)*timestep)
+
+plt.subplot(2,1,1)
+plt.plot(times, data[:, 0])
+plt.subplot(2,1,2)
+plt.plot(times, silences)
+plt.savefig('plot.png', dpi=1000)
+plt.show()
