a7b46f59d23588445ba85f0384bcc8f9ebe2e9d8
Ravi Shivam Autar <ravi_autar27@hotmail.com>
2017-11-05 00:15:31 +0100

    Update pip requirements and include pickle save commands

--- thermal_processor.py
+++ thermal_processor.py
@@ -1,19 +1,27 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
+import pickle
 import os
-from PIL import Image
-from collections import Counter
-import sklearn.preprocessing as preprocessing
-import png
+import sys
+from hurry.filesize import size
+from tqdm import tqdm
+from termcolor import colored
 
-images_path = "csv_images/"
-timestamps = pd.read_csv("project_data/thermal_timestamps.csv", index_col=0)
-labels = pd.read_csv("project_data/labels.csv", usecols=[0,1], dtype={"id": int, "label": str}, index_col=0)
-# labels
+thermal_folder = "thermal_images/"
+timestamp_folder = "project_data/thermal_timestamps.csv"
+label_folder = "project_data/labels.csv"
+
+filter_temp = 32
+total_num_frames = 40426
+
+timestamps = pd.read_csv(timestamp_folder, index_col=0)
+labels = pd.read_csv(label_folder, usecols=[0,1], dtype={"id": int, "label": str}, index_col=0)
+game = pd.read_csv(label_folder, usecols=[0,1], dtype={"id": int, "game": int}, index_col=0)
+
 def get_story_by_frame(subject, frame):
     second= frame/10.0 if (subject<15) else frame
     sub_data = timestamps.loc[subject].values
+    if (second < sub_data[0]): return 0
     i, j = 0, 1
     story = -1
     for k in range(3):
@@ -26,41 +34,69 @@ def get_story_by_frame(subject, frame):
 
 def get_label_by_frame(subject, frame):
     story = get_story_by_frame(subject, frame)
-    if (story == -1): return None
-    return labels.loc[subject].values[0][story-1]
+    if (story == 0 or story == -1): return (story, -1)
+    return (story, int(labels.loc[subject].values[0][story-1]))
 
-
-
-def csv_looper():
-    for root, dirs, files in os.walk("csv_images/"):
+def frame_with_info_traverse(thermal_path):
+    walker = os.walk(thermal_path)
+    walker.next() # skip the main dir
+    for root, dirs, files in walker:
+        if (len(files) == 0): continue # if the folder is empty or is root folder
+        subject = int(root.split("/")[-2].strip("Subject")) # thermal_images/Subject10/CSV images => 10
         for f in files:
-            yield pd.read_csv("{}/{}".format(images_path, f), header=None).as_matrix()
+            frame_nr = int(f.split("_")[1].strip(".csv")) # FL-000020_369.csv => 369
+            story, label = get_label_by_frame(subject, frame_nr) # get the story number and label of the frame
+            if (story == -1): continue # skip if the frame is between the stories
+            csv_frame = pd.read_csv(os.path.join(root, f)).as_matrix() # read the csv data
+            yield (subject, story, frame_nr, csv_frame, label)
+
+def extract_frame_features(frame_1):
+    frame_1[frame_1 < filter_temp] = 0 #filter all pixels lower than 30
+    frame_1 = frame_1[np.where(frame_1 != 0)].flatten() # new array with only filtered values
+    if len(frame_1) == 0:
+        return None
+    features = np.array([frame_1.min(), frame_1.max(), np.mean(frame_1)]) # get mean, max and min values of pixels
+    features = np.append(features, features[1] - features[0]) # add max difference in pixels
+    bin_places = np.histogram(frame_1, np.linspace(filter_temp, 37.0, 101))[0] # take the values of the bins
+    #     plot_bins(bin_places) # plot the bins if needed
+    features = np.concatenate((features, bin_places)) # add primary features to bin features
+    return features
 
 
-def save_as_image(image_matrix, name="temp.png"):
-    oldmin, oldmax= image_matrix.min(), image_matrix.max()
-    newmin, newmax= 0, 255
-    image_matrix = (((image_matrix-oldmin)*(newmax-newmin))/(oldmax-oldmin)) + newmin
-    image_matrix = map(lambda arr: map(lambda x: int(x), arr), image_matrix)
-    png.fromarray(image_matrix, 'L').save(name)
-# csv_generator = csv_looper()
-# frame_1 = csv_generator.next()
+csv_generator = frame_with_info_traverse(thermal_folder)
+baseline_count = dict(zip(range(1, 25), np.zeros(24)))
+baseline = dict(zip(range(1, 25), np.zeros(24)))
+control_group = {}
+test_group = {}
+# for _ in tqdm(range(4760)):
+#     csv_generator.next()
 
-frame_1 = pd.read_csv("SSP-000009_20.csv", header=None).as_matrix()
-frame_1[frame_1 < 30] = 0
-save_as_image(frame_1, name="arthur_filter.png")
-# frame_1 = frame_1[np.where(frame_1 != 0)].flatten()
-# features = np.array([frame_1.min(), frame_1.max(), np.mean(frame_1)])
-# features = np.append(features, features[1] - features[0])
-# bins_places = np.histogram(frame_1, np.linspace(30, 37.0, 101))[0]
-# features = np.concatenate((features, bins_places))
-# features = preprocessing.scale(features)
-# print features.mean(axis=0)
+for _ in tqdm(range(total_num_frames)):
+    subject, story, fnr, frame, label = csv_generator.next()
+    # print subject, story, fnr, frame, label
+    features = extract_frame_features(frame)
+    if features is None:
+        print colored((subject, story, fnr, frame, label), 'red')
+        continue
+    if (story==0):
+        baseline_count[subject] = baseline_count[subject] + 1
+        baseline[subject] = baseline[subject] + features
+        continue
+    update_dict = control_group if (game.loc[subject].values[0]==0) else test_group
+    if ((subject, story) not in update_dict):
+        update_dict[(subject, story)] = np.array([features, label])
+    else:
+        update_dict[(subject, story)] = np.vstack((update_dict[(subject, story)], [features, label]))
 
-# bins = np.linspace(30, 37.0, 200)
-# plt.hist(frame_1, bins)
-# plt.savefig('hist.png', dpi=1000)
-# plt.show()
 
+for key in baseline.keys():
+    if baseline_count[key]==0: continue
+    baseline[key] = baseline[key]/baseline_count[key]
 
+def serialize(group, fi):
+    with open(fi, 'wb') as f:
+        pickle.dump(group, f)
 
+serialize(control_group, "control_group.pickle")
+serialize(test_group, "test_group.pickle")
+serialize(baseline, "baseline.pickle")
